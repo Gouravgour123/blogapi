aa0a8b9f5b5fd1b448da247f7d8f2f8e80977d05
DotNetAmitSen <dotnetamitsen@gmail.com>
2024-01-24 15:09:15 +0530

    index

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
 const express = require("express");
 const app = express();
-const multer = require("multer")
 const cors = require("cors")
 require("./mongoose")
-const models = require("./blogModel")
+const models = require("./Models/blogModel");
+const { upload } = require("./multer");
+// const { model } = require("mongoose");
 app.use(express.json())
 app.use(cors())
 app.use(express.static("public"))
 
-const storage = multer.diskStorage({
-    destination:(req, file, cb)=>{
-        cb(null,"public/uploads/")
-    },
-    filename: (req, file, cb)=>{
-        cb(null,file.originalname);
-    }
-});
-const upload = multer({storage:storage}).single("image");
+const PORT = 5000;
+
 
 app.post("/", upload, async (req,res)=>{
     const data = await models({
@@ -33,12 +27,23 @@ app.get("/",  async (req,res)=>{
     
     res.send(data)
 })
-app.listen(5000);
 
 
+app.put('/update',upload,async (req,res)=>{
+    const data =await models.updateOne({name:req.body.name},{$set:{name:req.body.name,image:req.file.filename}})
+    // console.log(data)
+    res.send(data)
+})
 
-// app.get("/",  (req,res)=>{
-    
-//     res.send("data")
-// })
-// app.listen(4000);
+
+app.delete('/delete',upload, async (req,res)=>{
+    const data = await models.deleteOne({name:req.body.name})
+    console.log(data)
+    res.send(data)
+})
+
+
+app.listen(PORT, ()=>{
+    console.log(`server is running at ${PORT}`)
+});
+
